be22dae110435ef2ab94d5f87213fbb19d7a6f96
Rafael Sevilha <54326621+edorafa@users.noreply.github.com>
2020-08-11 10:48:03 -0300

    Update get_files_descriptions.py

--- task14 - conteudo_arquivos_glb/get_files_descriptions.py
+++ task14 - conteudo_arquivos_glb/get_files_descriptions.py
@@ -1,5 +1,16 @@
 from pygltflib import GLTF2
-glb_filename = "Cesium_Air.glb"
-glb = GLTF2().load(glb_filename)
+
+glb_filename1 = "pvgrid.glb"
+glb2 = GLTF2().load(glb_filename1)
+glb2.scene
+
+description1 = str(glb2)
+
+glb_filename2 = "Cesium_Air.glb"
+glb = GLTF2().load(glb_filename2)
 glb.scene
 
+description2 = str(glb)
+
+split_description1 = description1.split(', ')
+split_description2 = description2.split(', ')
